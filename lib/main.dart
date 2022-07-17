b572f5e581d0da602aceedaac2b5b1cd896f3030
HumYacom <thainakdee06@hotmail.com>
2022-07-17 15:17:29 +0700

    colors theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
   var app = MaterialApp(
     title: "App",
-    home: Text("Yep"),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Welcome Hee"),
+      ),
+      body: Text("ศาสนาแห่งกางเขนใน"),
+    ),
+    theme: ThemeData(primarySwatch: Colors.pink),
   );
   runApp(app);
 }
