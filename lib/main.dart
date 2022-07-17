ebd7eba001069896c755d419712da9879b3720d0
HumYacom <thainakdee06@hotmail.com>
2022-07-17 15:41:26 +0700

    input image+text fix

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,19 @@ class Main1 extends StatelessWidget {
     return MaterialApp(
       title: "App",
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Welcome Hee"),
-        ),
-        body: Text("ศาสนาแห่งกางเขนใน"),
-      ),
+          appBar: AppBar(
+            title: Text("Welcome Hee"),
+          ),
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                  "https://lzd-img-global.slatic.net/g/p/c5da016964721e0d47a069d538c464b5.jpg_720x720q80.jpg_.webp"),
+            ),
+            /*child: Text(
+              "ศาสนากางเขนใน",
+              style: TextStyle(fontSize: 30, color: Colors.red),
+          ),*/
+          )),
       theme: ThemeData(primarySwatch: Colors.pink),
     );
   }
